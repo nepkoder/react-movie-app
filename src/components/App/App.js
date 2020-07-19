c9e17f3e597f6ba754e068013932de65cbbe4823
Sujan Shrestha <nepkoder@gmail.com>
2020-07-19 23:24:10 +0545

    production ready deployment

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
     render() {
         return (
 
-            <BrowserRouter>
+            <BrowserRouter basename="/reactjs/">
             <React.Fragment>
 
 <Header/>
